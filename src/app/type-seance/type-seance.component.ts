130e4b56358cb21eac28049dbb25f791cec079e4
Ahlam <ahlamelassiri25@gmail.com>
2025-01-20 00:09:52 +0100

    designe

--- src/app/type-seance/type-seance.component.ts
+++ src/app/type-seance/type-seance.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-type-seance',
   templateUrl: './type-seance.component.html',
+  standalone: true,
   styleUrls: ['./type-seance.component.css']
 })
 export class TypeSeanceComponent {
