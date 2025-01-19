09832bb2dd231670a1d0d6d6e0dece2b5e7c0694
Ahlam <ahlamelassiri25@gmail.com>
2025-01-19 20:59:56 +0100

    modif routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -16,32 +16,36 @@ import {FormMatiereComponent} from "./form-matiere/form-matiere.component";
 import {FormTypeSceanceComponent} from "./form-type-sceance/form-type-sceance.component";
 import {FormSalleComponent} from "./form-salle/form-salle.component";
 import {FormDurationComponent} from "./form-duration/form-duration.component";
-
 const routes: Routes = [
-
   { path: 'filiere', component: FilliereComponent },
   { path: 'filiere/form', component: FiliereFormComponent },
   { path: 'filiere/edit/:id', component: FiliereFormComponent },
 
-
-  { path: 'profs', component: ProfsComponent},
-  { path: 'profs/form', component:FormProfComponent  },
+  { path: 'profs', component: ProfsComponent },
+  { path: 'profs/form', component: FormProfComponent },
+  { path: 'profs/edit/:id', component: FormProfComponent },
 
   { path: 'matieres', component: MatieresComponent },
-  { path: 'matieres/form', component: FormMatiereComponent},
+  { path: 'matieres/form', component: FormMatiereComponent },
+  { path: 'matieres/edit/:id', component: FormMatiereComponent },
 
   { path: 'type_seance', component: TypeSeanceComponent },
-  { path: 'type_seance/form', component: FormTypeSceanceComponent},
+  { path: 'type_seance/form', component: FormTypeSceanceComponent },
+  { path: 'type_seance/edit/:id', component: FormTypeSceanceComponent },
 
   { path: 'salles', component: SallesComponent },
   { path: 'salles/form', component: FormSalleComponent },
+  { path: 'salles/edit/:id', component: FormSalleComponent },
 
   { path: 'duration', component: DurationComponent },
   { path: 'duration/form', component: FormDurationComponent },
+  { path: 'duration/edit/:id', component: FormDurationComponent },
 
   { path: 'emploi-du-temps', component: EmploiDuTempsComponent },
   { path: 'schedule-form', component: ScheduleFormComponent },
+  { path: 'schedule/edit/:id', component: ScheduleFormComponent }
 ];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
